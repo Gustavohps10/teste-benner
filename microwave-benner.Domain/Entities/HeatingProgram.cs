b8f802023db775b55dc312a14ac36a7dcb7b1218
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-05 17:55:59 -0300

    feat: set domain entities

---
+++ microwave-benner.Domain/Entities/HeatingProgram.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Xml.Linq;
+
+namespace microwave_benner.Domain.Entities
+{
+    public class HeatingProgram
+    {
+        public int id { get; private set; }
+        public string name { get; private set; }
+        public string food { get; private set; }
+        public int time { get; private set; }
+        public int power { get; private set; }
+        public string? instructions { get; private set; }
+        public bool custom { get; private set; }
+
+        public HeatingProgram(string name, string food, int time, int power, string? instructions = null, bool custom = false)
+        {
+            this.name = name;
+            this.food = food;
+            this.time = time;
+            this.power = power;
+            this.instructions = instructions;
+            this.custom = custom;
+        }
+
+        public HeatingProgram(int id, string name, string food, int time, int power, string? instructions = null, bool custom = false)
+        {
+            this.id = id;
+            this.name = name;
+            this.food = food;
+            this.time = time;
+            this.power = power;
+            this.instructions = instructions;
+            this.custom = custom;
+        }
+    }
+}
---
+++ microwave-benner.Domain/Entities/HeatingTask.cs
@@ -0,0 +1,25 @@
+﻿namespace microwave_benner.Domain.Entities
+{
+    public class HeatingTask
+    {
+        public int id { get; private set; }
+        public int time { get; private set; }
+        public int power { get; private set; }
+        public DateTime? startTime { get; private set; }
+        public DateTime? pauseTime { get; private set; }
+        public DateTime? endTime { get; private set; }
+        public int? heatingProgramId { get; private set; }
+
+
+        // Constructor
+        public HeatingTask(int time, int power, DateTime? startTime = null, DateTime? pauseTime = null, DateTime? endTime = null, int? heatingProgramId = null)
+        {
+            this.time = time;
+            this.power = power;
+            this.startTime = startTime;
+            this.pauseTime = pauseTime;
+            this.endTime = endTime;
+            this.heatingProgramId = heatingProgramId;
+        }
+    }
+}
