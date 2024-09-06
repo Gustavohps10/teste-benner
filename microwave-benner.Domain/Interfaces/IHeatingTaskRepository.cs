517d6b986680408522285aa1d644bde154548cba
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-05 22:25:53 -0300

    feat: add HeatingTask repository

---
+++ microwave-benner.Domain/Interfaces/IHeatingTaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using microwave_benner.Domain.Entities;
+
+namespace microwave_benner.Domain.Interfaces
+{
+    public interface IHeatingTaskRepository
+    {
+        Task<IEnumerable<HeatingTask>> GetAll();
+        Task<HeatingTask> GetById(int id);
+        Task Insert(HeatingTask heatingTask);
+        Task Update(HeatingTask heatingTask);
+    }
+}
---
+++ microwave-benner.Infra.Data/Repositories/HeatingTaskRepository.cs
@@ -0,0 +1,46 @@
+﻿using microwave_benner.Domain.Entities;
+using microwave_benner.Domain.Interfaces;
+using microwave_benner.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace microwave_benner.Infra.Data.Repositories
+{
+    public class HeatingTaskRepository : IHeatingTaskRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HeatingTaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<HeatingTask> GetById(int id)
+        {
+            HeatingTask? heatingTask = await _context.HeatingTasks.FindAsync(id);
+            if (heatingTask == null)
+            {
+                throw new KeyNotFoundException("Tarefa de aquecimento não encontrada.");
+            }
+            return heatingTask;
+        }
+
+        public async Task<IEnumerable<HeatingTask>> GetAll()
+        {
+            return await _context.HeatingTasks.ToListAsync();
+        }
+
+        public async Task Insert(HeatingTask heatingTask)
+        {
+            await _context.HeatingTasks.AddAsync(heatingTask);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(HeatingTask heatingTask)
+        {
+            _context.HeatingTasks.Update(heatingTask);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
