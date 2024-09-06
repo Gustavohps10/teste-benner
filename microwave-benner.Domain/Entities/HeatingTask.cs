04c650fd3510f74a8fb61fbaad1bed1fb1317b01
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-06 03:07:34 -0300

    feat: add methods to control HeatingTask

--- microwave-benner.Domain/Entities/HeatingTask.cs
+++ microwave-benner.Domain/Entities/HeatingTask.cs
@@ -10,14 +10,71 @@
         public DateTime? endTime { get; private set; }
         //public int? heatingProgramId { get; private set; }
 
-        public HeatingTask(int time, int power, DateTime? startTime = null, DateTime? pauseTime = null, DateTime? endTime = null)
+        // Inicio Rápido
+        private const int defaultTime = 30; // Segundos
+        private const int defaultPower = 10;
+
+        public HeatingTask(int time = defaultTime, int power = defaultPower)
         {
-            this.time = time;
-            this.power = power;
+            this.time = time > 0 ? time : defaultTime;
+            this.power = power > 0 ? power : defaultPower;
+        }
+
+        public HeatingTask(int id, int time, int power, DateTime? startTime = null, DateTime? pauseTime = null, DateTime? endTime = null)
+            : this(time, power)
+        {
+            this.id = id;
             this.startTime = startTime;
             this.pauseTime = pauseTime;
             this.endTime = endTime;
-            //this.heatingProgramId = heatingProgramId;
+        }
+
+        public void Start()
+        {
+            if (this.startTime.HasValue)
+            {
+                throw new InvalidOperationException("O aquecimento já foi iniciado.");
+            }
+            this.startTime = DateTime.UtcNow;
+        }
+
+        public void Pause()
+        {
+            if (!this.startTime.HasValue)
+            {
+                throw new InvalidOperationException("O aquecimento não foi iniciado.");
+            }
+            if (this.pauseTime.HasValue)
+            {
+                throw new InvalidOperationException("O aquecimento já foi pausado.");
+            }
+            this.pauseTime = DateTime.UtcNow;
+        }
+
+        public void Continue()
+        {
+            if (!this.pauseTime.HasValue)
+            {
+                throw new InvalidOperationException("O aquecimento não está pausado.");
+            }
+            if (this.endTime.HasValue)
+            {
+                throw new InvalidOperationException("O aquecimento já foi finalizado.");
+            }
+            this.pauseTime = null;
+        }
+
+        public void Interrupt()
+        {
+            if (!this.startTime.HasValue)
+            {
+                throw new InvalidOperationException("O aquecimento não foi iniciado.");
+            }
+            if (this.endTime.HasValue)
+            {
+                throw new InvalidOperationException("O aquecimento já foi finalizado.");
+            }
+            this.endTime = DateTime.UtcNow;
         }
     }
 }
