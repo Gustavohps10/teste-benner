524ba96e0bbc81232c528bd23d7fe1e0b1f61ff9
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-06 04:26:21 -0300

    feat: add Start HeatingTask service

---
+++ microwave-benner.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using microwave_benner.Application.DTOs;
+using microwave_benner.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace microwave_benner.Application.Mappings
+{
+    public class DomainToDTOMappingProfile: Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<HeatingTask, HeatingTaskDTO>().ReverseMap();
+        }
+    }
+}
---
+++ microwave-benner.Application/Services/StartHeatingTaskService.cs
@@ -0,0 +1,34 @@
+﻿using microwave_benner.Application.UseCases;
+using microwave_benner.Domain.Entities;
+using microwave_benner.Domain.Interfaces;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using microwave_benner.Application.DTOs;
+
+namespace microwave_benner.Application.Services
+{
+    public class StartHeatingTaskService : IStartHeatingTaskUseCase
+    {
+        private readonly IHeatingTaskRepository _heatingTaskRepository;
+        private readonly IMapper _mapper;
+
+        public StartHeatingTaskService(IHeatingTaskRepository heatingTaskRepository, IMapper mapper)
+        {
+            _heatingTaskRepository = heatingTaskRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Execute(HeatingTaskDTO heatingTaskDTO)
+        {
+
+            HeatingTask heatingTask = _mapper.Map<HeatingTask>(heatingTaskDTO);
+            heatingTask.Start();
+
+            await _heatingTaskRepository.Insert(heatingTask);
+        }
+    }
+}
