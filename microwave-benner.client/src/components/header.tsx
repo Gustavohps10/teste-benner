2472ccf96cb918caf15e9afc86402377e806d547
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-07 06:28:47 -0300

    feat(ui): add toggle theme

--- microwave-benner.client/src/components/header.tsx
+++ microwave-benner.client/src/components/header.tsx
@@ -1,8 +1,11 @@
+import { ToggleTheme } from "./toggle-theme";
+
 export function Header() {
   return (
     <header className="shadow-md">
-      <div className="flex mx-auto max-w-[90rem]">
-        
+      <div className="flex mx-auto max-w-[90rem] items-center py-4 gap-2 justify-between">
+        <div>Menu</div>
+        <ToggleTheme/>
       </div>
     </header>
   )
