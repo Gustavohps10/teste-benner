e82dab60eb352c43fad2107782340b95709b969e
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-07 07:07:48 -0300

    feat(ui): add microwave base style

--- microwave-benner.client/src/pages/home.tsx
+++ microwave-benner.client/src/pages/home.tsx
@@ -1,3 +1,31 @@
+import { Button } from "@/components/ui/button"
+import { Slider } from "@/components/ui/slider"
+
 export function Home() {
-  return <>Home Page</>
+  return <>
+    <div className="border rounded-md w-[900px] h-[500px] mx-auto my-20 flex">
+
+      <div className=" h-full flex-1 p-6">
+          <div className="border w-full h-full rounded-md"></div>
+      </div>
+
+      <div className="border-l w-[280px] p-6">
+        <div className="border rounded-md w-full h-24"></div>
+
+        <div className="grid grid-cols-3 justify-center gap-3 py-4">
+          {Array.from({length: 9}).map((_, index)=>
+            <Button key={index} size="lg" variant="secondary">{index+1}</Button>
+          )}
+          <Button size="lg" variant="secondary">0</Button>
+          <Button size="lg" variant="secondary">Play</Button>
+          <Button size="lg" variant="secondary">Cancel</Button>
+        </div>
+
+        <div>
+          <span className="text-muted-foreground text-sm">Potência</span>
+          <Slider className="mt-2" min={1} max={10}/>
+        </div>
+      </div>
+    </div>
+  </>
 }
