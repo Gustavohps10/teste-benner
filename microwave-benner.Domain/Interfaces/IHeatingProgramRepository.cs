f3e0e190d991d4db1d99536fd9909d3ef2990c7d
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-06 21:38:13 -0300

    feat(repository): add HeatingProgram repository implementation

---
+++ microwave-benner.Domain/Interfaces/IHeatingProgramRepository.cs
@@ -0,0 +1,14 @@
+﻿using microwave_benner.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace microwave_benner.Domain.Interfaces
+{
+    public interface IHeatingProgramRepository
+    {
+        Task<IEnumerable<HeatingProgram>> GetAll();
+        Task<HeatingProgram> GetById(int id);
+        Task Insert(HeatingProgram heatingProgram);
+        Task Update(HeatingProgram heatingProgram);
+    }
+}
---
+++ microwave-benner.Infra.Data/Repositories/HeatingProgramRepository,cs.cs
@@ -0,0 +1,46 @@
+﻿using microwave_benner.Domain.Entities;
+using microwave_benner.Domain.Interfaces;
+using microwave_benner.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace microwave_benner.Infra.Data.Repositories
+{
+    public class HeatingProgramRepository : IHeatingProgramRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HeatingProgramRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<HeatingProgram>> GetAll()
+        {
+            return await _context.HeatingPrograms.ToListAsync();
+        }
+
+        public async Task<HeatingProgram> GetById(int id)
+        {
+            HeatingProgram? heatingProgram = await _context.HeatingPrograms.FindAsync(id);
+            if (heatingProgram == null)
+            {
+                throw new KeyNotFoundException("Programa de aquecimento não encontrado.");
+            }
+            return heatingProgram;
+        }
+
+        public async Task Insert(HeatingProgram heatingProgram)
+        {
+            await _context.HeatingPrograms.AddAsync(heatingProgram);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(HeatingProgram heatingProgram)
+        {
+            _context.HeatingPrograms.Update(heatingProgram);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
